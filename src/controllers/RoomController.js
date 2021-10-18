f2de7117b7c879058b7595cd435a2256711659e0
Guilherme Moura <guilhermermribeiro@gmail.com>
2021-10-18 09:02:50 -0300

    add gerador de id dinamico adicionando no DB

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,7 +1,26 @@
-module.exports = {
-    create(req, res){
+const Database = require("../db/config")
 
-        let roomId = 21321;
+module.exports = {
+    async create(req, res){
+        const db = await Database();
+        const pass = req.body.passward;
+        let roomId;
+
+        for(var i = 0; i <6; i++){
+            i == 0 ? roomId = Math.floor(Math.random() * 10).toString() : 
+            roomId += Math.floor(Math.random() * 10).toString();
+        }
+        
+        await db.run(`INSERT INTO rooms (
+            id,
+            pass
+        ) VALUES (
+            ${parseInt(roomId)},
+            ${pass}
+        )`)
+    
+        await db.close();
+    
 
         res.redirect(`/room/${roomId}`);
     }
